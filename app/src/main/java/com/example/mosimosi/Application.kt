be1457e42d312208fc18ae76fce259b4fcf8a30b
hyunhomon <hyunhomon@gmail.com>
2023-12-16 15:04:16 +0900

    init

---
+++ app/src/main/java/com/example/mosimosi/Application.kt
@@ -0,0 +1,13 @@
+package com.example.mosimosi
+
+import android.app.Application
+
+class Application : Application() {
+    companion object {
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
